2a7fff5821076d91e3d9a72db5903ea4a9018716
YavizGuldalf <skeletrox1@hotmail.com>
2023-01-28 02:50:41 +0100

    Fix the issue related to static variables. Static global variables are private to each file, so the definitions and changes made in tests.c were not visible to LICs.c file. Therefore all uses of static variables have been replaced by their non-static counterparts in tests.c

--- tests.c
+++ tests.c
@@ -8,30 +8,28 @@ boolean check_lic_0(void);
 int tests_run = 0;
 
 static char * test_lic0_positive(){
-    NUMPOINTS2 = 10;
-    PARAMETERS2.LENGTH1 = 1;
+    NUMPOINTS = 10;
+    PARAMETERS.LENGTH1 = 1;
     double local_X[5];
-    X2 = local_X;
+    X = local_X;
     double local_Y[5];
-    Y2 = local_Y;
-    X2[0]=1; X2[1]=2; X2[2]=3; X2[3]=12; X2[4]=17;
-    Y2[0]=1; Y2[1]=2; Y2[2]=3; Y2[3]=12; Y2[4]=22;
-
-    printf("%d\n", check_lic_0());
+    Y = local_Y;
+    X[0]=1; X[1]=2; X[2]=3; X[3]=12; X[4]=17;
+    Y[0]=1; Y[1]=2; Y[2]=3; Y[3]=12; Y[4]=22;
 
     mu_assert("The positive test failed!", check_lic_0() == true);
     return 0;
 }
 
 static char * test_lic0_negative(){
-    NUMPOINTS2 = 5;
-    PARAMETERS2.LENGTH1 = 3;
+    NUMPOINTS = 5;
+    PARAMETERS.LENGTH1 = 3;
     double local_X[5];
-    X2 = local_X;
+    X = local_X;
     double local_Y[5];
-    Y2 = local_Y;
-    X2[0]=1; X2[1]=2; X2[2]=3; X2[3]=4; X2[4]=5;
-    Y2[0]=1; Y2[1]=2; Y2[2]=3; Y2[3]=4; Y2[4]=6;
+    Y = local_Y;
+    X[0]=1; X[1]=2; X[2]=3; X[3]=4; X[4]=5;
+    Y[0]=1; Y[1]=2; Y[2]=3; Y[3]=4; Y[4]=6;
 
     mu_assert("The negative test failed!", check_lic_0() == false);
     return 0;

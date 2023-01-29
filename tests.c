2d6089812db557597b7955f336961750a87bd9bf
Pontus <pontuspr@kth.se>
2023-01-29 14:27:53 +0100

    feature: added functions for testing only lic0 or lic5

--- tests.c
+++ tests.c
@@ -134,40 +134,6 @@ static char * test_lic5_invalid(){
     return 0;
 }
 
-static char * test_lic5_positive(){
-    NUMPOINTS = 2;
-    double local_X[2];
-    X = local_X;
-    double local_Y[2];
-    Y = local_Y;
-    X[0]=2; X[1]=1;
-    Y[0]=1; Y[1]=2;
-
-    mu_assert("The positive test for LIC 5 failed!", check_lic_5() == true);
-    return 0;
-}
-
-static char * test_lic5_negative(){
-    NUMPOINTS = 2;
-    double local_X[2];
-    X = local_X;
-    double local_Y[2];
-    Y = local_Y;
-    X[0]=2; X[1]=10;
-    Y[0]=1; Y[1]=2;
-
-    mu_assert("The negative test for LIC 5 failed!", check_lic_5() == false);
-    return 0;
-}
-
-static char * test_lic5_invalid(){
-    NUMPOINTS = 2;
-    X = NULL;
-    Y = NULL;
-    mu_assert("The invalid test for LIC 5 failed!", check_lic_5() == false);
-    return 0;
-}
-
 /*
 *  A test where check_lic_7 should return true
 * @return 0 if the test passes, an error message otherwise
@@ -247,6 +213,28 @@ static char * current_tests() {
 }
 
 /*
+*  Runs all of the tests of lic0 until an error is encountered or all the tests are passed.
+*  @returns 0 if all the tests pass, the error message of the first test that fails otherwise.
+*/
+static char * lic0_tests(){
+    mu_run_test(test_lic0_negative);
+    mu_run_test(test_lic0_positive);
+    mu_run_test(test_lic0_invalid);
+    return 0;
+}
+
+/*
+*  Runs all of the tests of lic7 until an error is encountered or all the tests are passed.
+*  @returns 0 if all the tests pass, the error message of the first test that fails otherwise.
+*/
+static char * lic5_tests(){
+    mu_run_test(test_lic5_negative);
+    mu_run_test(test_lic5_positive);
+    mu_run_test(test_lic5_invalid);
+    return 0;
+}
+
+/*
 *  Runs all of the tests of lic7 until an error is encountered or all the tests are passed.
 *  @returns 0 if all the tests pass, the error message of the first test that fails otherwise.
 */
@@ -258,7 +246,7 @@ static char * lic7_tests(){
 }
 
 int main(int argc, char **argv) {
-    char *result = lic7_tests();
+    char *result = lic5_tests();
     if (result != 0) {
         printf("%s\n", result);
     }
